f7a42fa8ff6458cc4c4b68a544856a92e272c890
Ayush15601 <aayushtewarit@gmail.com>
2026-05-31 18:42:28 +0530

    added required images and chat gpt logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./assets/css/App.css"
+import gptlogo from "./assets/chatgpt.svg"
 
 function App() {
 
@@ -11,6 +12,14 @@ function App() {
         <div className="sidebar">
 
           <div className="sidebar_up">
+
+            <div className="image"> 
+
+              <img src={gptlogo} alt="cgat gpt image" className="a_image" />
+
+              <span> Chat GPT </span>
+
+            </div>
 
           </div>
 
